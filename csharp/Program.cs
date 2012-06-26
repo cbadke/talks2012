0352ccc43c88cd7898c4e629bed75ef636a95d22
Curtis Badke <curtis.badke@devfacto.com>
2012-06-25 19:45:21 -0600

    Start higher order function demo

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -6,11 +6,41 @@ using System.Threading.Tasks;
 
 namespace Demo
 {
+
+    class Sort
+    {
+        public static IEnumerable<int> InsertionSort(IEnumerable<int> list)
+        {
+            var newList = new List<int>();
+
+            foreach (var thing in list)
+            {
+                int i = 0;
+                while (i < newList.Count() && thing >= newList[i])
+                {
+                    ++i;
+                }
+                newList.Insert(i, thing);
+            }
+
+            return newList;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-		Console.WriteLine("Hello world");
+            List<int> list = new List<int>() {4, 3, 1, 2};
+
+            List<int> sorted = Sort.InsertionSort(list).ToList();
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
