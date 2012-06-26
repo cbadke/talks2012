6f407aca55ce5a03285fc0998e186d554070de60
Curtis Badke <curtis.badke@devfacto.com>
2012-06-26 00:22:51 -0600

    Completed ticker example

--- fsharp/ticker.fsx
+++ fsharp/ticker.fsx
@@ -9,3 +9,10 @@ let getPrice ticker =
   Double.Parse(dataItems.[dataItems.Length - 1])
 
 let tickers = ["AAPL"; "AMD"; "CSCO"; "GOOG"; "HPQ"; "INTC"; "MSFT"; "ORCL"; "QCOM"; "XRX"]
+
+tickers
+ |> List.map (fun t -> (t, (getPrice t)))
+ |> List.filter (fun tp -> snd(tp) < 500.0)
+ |> List.reduce (fun tp1 tp2 -> if snd(tp1) > snd(tp2) then tp1 else tp2)
+ |> printfn "%O"
+
